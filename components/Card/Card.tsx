b638b391a4618f7d06a5b67f9b3939953cd4cf4f
nathanhleung <github@nathanhleung.com>
2022-02-24 16:48:32 -0800

    Make Card variant optional

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,7 +1,7 @@
 import { Box, BoxProps, useStyleConfig } from "@chakra-ui/react";
 
 type CardProps = BoxProps & {
-  variant: string;
+  variant?: string;
 };
 
 const Card: React.FC<CardProps> = ({ variant, ...restProps }) => {
