354b943130bf73e4d4a95839166a210f5e3e719a
nathanhleung <github@nathanhleung.com>
2022-02-24 16:29:09 -0800

    Add link to clarifying issue

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -5,6 +5,7 @@ type CardProps = BoxProps & {
 };
 
 const Card: React.FC<CardProps> = ({ variant, ...restProps }) => {
+  // See https://github.com/chakra-ui/chakra-ui/issues/2456 for more info
   const styles = useStyleConfig("Card", { variant });
 
   return <Box __css={styles} borderRadius="md" padding={6} {...restProps} />;
