c0e22b23da74ca49975204e8b5922782c617c305
nathanhleung <github@nathanhleung.com>
2022-02-24 17:38:34 -0800

    Increase Card padding

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -8,7 +8,7 @@ const Card: React.FC<CardProps> = ({ variant, ...restProps }) => {
   // See https://github.com/chakra-ui/chakra-ui/issues/2456 for more info
   const styles = useStyleConfig("Card", { variant });
 
-  return <Box __css={styles} borderRadius="md" padding={6} {...restProps} />;
+  return <Box __css={styles} borderRadius="md" padding={8} {...restProps} />;
 };
 
 export default Card;
