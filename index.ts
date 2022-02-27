73d49ad84066472cfb6c133f1a56b628e63acaf9
nathanhleung <github@nathanhleung.com>
2022-02-26 22:25:49 -0800

    Export new components

--- index.ts
+++ index.ts
@@ -1,21 +1,25 @@
 import Button from "./components/Button/Button";
 import Card from "./components/Card/Card";
+import ExpandableCard from "./components/ExpandableCard/ExpandableCard";
 import Heading from "./components/Heading/Heading";
 import Input from "./components/Input/Input";
 import Link from "./components/Link/Link";
 import Statistic from "./components/Statistic/Statistic";
 import Text from "./components/Text/Text";
+import TokenAmountInput from "./components/TokenAmountInput/TokenAmountInput";
 import TokenIcon from "./components/TokenIcon/TokenIcon";
 import TokenGroup from "./components/TokenGroup/TokenGroup";
 
 export {
   Button,
   Card,
+  ExpandableCard,
   Heading,
   Input,
   Link,
   Statistic,
   Text,
+  TokenAmountInput,
   TokenIcon,
   TokenGroup,
 };
