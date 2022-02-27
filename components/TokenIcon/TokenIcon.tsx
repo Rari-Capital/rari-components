7700e96130457d2950d3e94f86d6d42fad355b69
nathanhleung <github@nathanhleung.com>
2022-02-26 22:44:44 -0800

    Fail gracefully when address is invalid

--- components/TokenIcon/TokenIcon.tsx
+++ components/TokenIcon/TokenIcon.tsx
@@ -9,10 +9,12 @@ const TokenIcon: React.FC<TokenIconProps> = ({
   tokenAddress,
   ...restProps
 }) => {
-  const checksumTokenAddress = ethers.utils.getAddress(tokenAddress);
-
-  // Load icon from trustwallet/assets via jsdelivr
-  const logoUrl = `https://cdn.jsdelivr.net/gh/trustwallet/assets@b75e0805b39b743cb25dd09561cc85777209ed84/blockchains/ethereum/assets/${checksumTokenAddress}/logo.png`;
+  let logoUrl = "";
+  try {
+    const checksumTokenAddress = ethers.utils.getAddress(tokenAddress);
+    // Load icon from trustwallet/assets via jsdelivr
+    logoUrl = `https://cdn.jsdelivr.net/gh/trustwallet/assets@b75e0805b39b743cb25dd09561cc85777209ed84/blockchains/ethereum/assets/${checksumTokenAddress}/logo.png`;
+  } catch {}
 
   return <Avatar src={logoUrl} {...restProps} />;
 };
