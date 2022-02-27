ec4cd80a9f30ce107365ca86ef60864eccd9e928
nathanhleung <github@nathanhleung.com>
2022-02-26 21:05:25 -0800

    Add ExpandableCard

---
+++ components/ExpandableCard/ExpandableCard.stories.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import Statistic from "../Statistic/Statistic";
+import ExpandableCard from "./ExpandableCard";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Rari/ExpandableCard",
+  component: ExpandableCard,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    fontSize: {
+      control: "select",
+      options: [
+        "xs",
+        "sm",
+        "md",
+        "lg",
+        "xl",
+        "2xl",
+        "3xl",
+        "4xl",
+        "5xl",
+        "6xl",
+      ],
+    },
+  },
+} as ComponentMeta<typeof ExpandableCard>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof ExpandableCard> = (args) => (
+  <ExpandableCard {...args} />
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  children: "ExpandableCard",
+  expandableChildren: <p>Expandable content</p>,
+};
+
+export const Light = Template.bind({});
+Light.args = {
+  children: "Light ExpandableCard",
+  expandableChildren: <p>Expandable content</p>,
+  variant: "light",
+};
---
+++ components/ExpandableCard/ExpandableCard.tsx
@@ -0,0 +1,76 @@
+import { TriangleDownIcon, TriangleUpIcon } from "@chakra-ui/icons";
+import {
+  Accordion,
+  AccordionButton,
+  AccordionItem,
+  AccordionPanel,
+  Box,
+  Divider,
+  Flex,
+  IconButton,
+  useStyleConfig,
+} from "@chakra-ui/react";
+
+import Card from "../Card/Card";
+
+type ExpandableCardProps = React.ComponentProps<typeof Card> & {
+  expandableChildren: React.ReactNode;
+};
+
+const ExpandableCard: React.FC<ExpandableCardProps> = ({
+  children,
+  expandableChildren,
+  variant,
+  ...restProps
+}) => {
+  const { color } = useStyleConfig("Card", { variant }) as { color: string };
+
+  return (
+    <Accordion allowToggle>
+      <AccordionItem borderY="none">
+        {({ isExpanded }) => (
+          <Card variant={variant} {...restProps}>
+            <AccordionButton
+              padding={0}
+              _hover={{ background: "transparent" }}
+              _focus={{
+                outline: "none",
+              }}
+            >
+              <Flex
+                alignItems="center"
+                justifyContent="space-between"
+                width="100%"
+              >
+                <Box>{children}</Box>
+                <IconButton
+                  aria-label="toggle"
+                  _focus={{
+                    outline: "none",
+                  }}
+                  bg="transparent"
+                  _hover={{
+                    bg: "transparent",
+                  }}
+                  _active={{
+                    bg: "transparent",
+                  }}
+                  color={color}
+                  icon={isExpanded ? <TriangleUpIcon /> : <TriangleDownIcon />}
+                />
+              </Flex>
+            </AccordionButton>
+            <AccordionPanel padding={0}>
+              <Box paddingY={8}>
+                <Divider borderColor={color} opacity={0.1} />
+              </Box>
+              {expandableChildren}
+            </AccordionPanel>
+          </Card>
+        )}
+      </AccordionItem>
+    </Accordion>
+  );
+};
+
+export default ExpandableCard;
