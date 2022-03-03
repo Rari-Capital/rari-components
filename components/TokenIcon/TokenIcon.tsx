50cc0fb7fa37095fd3d78836d2ccabe272f96bf1
nathanhleung <github@nathanhleung.com>
2022-03-02 21:28:16 -0800

    Use Rari API to get token URLs

--- components/TokenIcon/TokenIcon.tsx
+++ components/TokenIcon/TokenIcon.tsx
@@ -1,5 +1,6 @@
 import { Avatar, AvatarProps } from "@chakra-ui/avatar";
 import { ethers } from "ethers";
+import { useEffect, useState } from "react";
 
 type TokenIconProps = AvatarProps & {
   tokenAddress: string;
@@ -13,12 +14,25 @@ const TokenIcon: React.FC<TokenIconProps> = ({
   tokenAddress,
   ...restProps
 }) => {
-  let logoUrl = "";
-  try {
-    const checksumTokenAddress = ethers.utils.getAddress(tokenAddress);
-    // Load icon from trustwallet/assets via jsdelivr
-    logoUrl = `https://cdn.jsdelivr.net/gh/trustwallet/assets@b75e0805b39b743cb25dd09561cc85777209ed84/blockchains/ethereum/assets/${checksumTokenAddress}/logo.png`;
-  } catch {}
+  // let logoUrl = "";
+  // try {
+  //   const checksumTokenAddress = ethers.utils.getAddress(tokenAddress);
+  //   // Load icon from trustwallet/assets via jsdelivr
+  //   logoUrl = `https://cdn.jsdelivr.net/gh/trustwallet/assets@b75e0805b39b743cb25dd09561cc85777209ed84/blockchains/ethereum/assets/${checksumTokenAddress}/logo.png`;
+  // } catch {}
+
+  const [logoUrl, setLogoUrl] = useState("");
+  useEffect(() => {
+    async function getData() {
+      // Use Rari api to get token logo url
+      const dataUrl = `https://rari-git-l2tokendata-rari-capital.vercel.app/api/tokenData?address=${tokenAddress.toLowerCase()}&chainId=1`;
+      const data = await fetch(dataUrl);
+      const json = await data.json();
+      setLogoUrl(json.logoURL);
+    }
+
+    getData();
+  }, [tokenAddress]);
 
   return <Avatar src={logoUrl} {...restProps} />;
 };
