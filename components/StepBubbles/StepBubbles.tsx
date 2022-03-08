3eba088ea2530a716e47609aac0c2363969eac6f
Koan <80938373+cryptickoan@users.noreply.github.com>
2022-03-08 16:04:28 -0600

    Show loading/complete state on each step in `StepBubble` (#5)
    Co-authored-by: nathanhleung <github@nathanhleung.com>

--- components/StepBubbles/StepBubbles.tsx
+++ components/StepBubbles/StepBubbles.tsx
@@ -1,4 +1,4 @@
-import { Box, Center, Flex } from "@chakra-ui/react";
+import { Box, Center, Flex, Spinner } from "@chakra-ui/react";
 import Text from "../Text/Text";
 
 type StepBubbleProps = {
@@ -10,13 +10,22 @@ type StepBubbleProps = {
    * Whether this bubble is the last in line and should not have a connector.
    */
   last?: boolean;
+  /**
+   * The background color for the bubble.
+   */
+  background: string;
 };
 
-const StepBubble: React.FC<StepBubbleProps> = ({ children, active, last }) => {
+const StepBubble: React.FC<StepBubbleProps> = ({
+  children,
+  active,
+  last,
+  background,
+}) => {
   return (
     <Center>
       <Center
-        background="primary"
+        background={background}
         height={12}
         width={12}
         borderRadius="50%"
@@ -24,7 +33,7 @@ const StepBubble: React.FC<StepBubbleProps> = ({ children, active, last }) => {
       >
         {children}
       </Center>
-      {!last && <Box width={12} height="1px" background="primary" />}
+      {!last && <Box width={12} height="1px" background={background} />}
     </Center>
   );
 };
@@ -39,6 +48,11 @@ type StepBubblesProps = {
    * etc.).
    */
   activeIndex: number;
+  /**
+   * Whether to display a loading state (shows a spinner on the currently active
+   * step).
+   */
+  loading?: boolean;
 };
 
 /**
@@ -46,14 +60,32 @@ type StepBubblesProps = {
  * connected by a line, with a single circle highlighted denoting an active
  * step.
  */
-const StepBubbles: React.FC<StepBubblesProps> = ({ steps, activeIndex }) => {
+const StepBubbles: React.FC<StepBubblesProps> = ({
+  steps,
+  activeIndex,
+  loading,
+}) => {
   return (
     <Flex>
-      {Array.from({ length: steps }).map((_, i) => (
-        <StepBubble key={i} active={i === activeIndex} last={i === steps - 1}>
-          <Text fontWeight={600}>{i + 1}</Text>
-        </StepBubble>
-      ))}
+      {Array.from({ length: steps }).map((_, i) => {
+        const active = i === activeIndex;
+
+        return (
+          <StepBubble
+            key={i}
+            // Show a gray background if the step was completed
+            background={i < activeIndex ? "gray" : "primary"}
+            active={active}
+            last={i === steps - 1}
+          >
+            {active && loading ? (
+              <Spinner />
+            ) : (
+              <Text fontWeight={600}>{i + 1}</Text>
+            )}
+          </StepBubble>
+        );
+      })}
     </Flex>
   );
 };
