f7ed0e93cbd2e20449cf5b542f7ce921c8703128
nathanhleung <github@nathanhleung.com>
2022-03-28 21:39:50 -0700

    Export types from index.ts

--- index.ts
+++ index.ts
@@ -1,24 +1,30 @@
-import Badge from "./components/Badge";
-import Button from "./components/Button";
-import Card from "./components/Card";
-import Divider from "./components/Divider";
-import ExpandableCard from "./components/ExpandableCard";
-import Heading from "./components/Heading";
-import HoverableCard from "./components/HoverableCard";
-import Input from "./components/Input";
-import Link from "./components/Link";
-import Modal from "./components/Modal";
-import Progress from "./components/Progress";
-import Statistic from "./components/Statistic";
-import StatisticTable from "./components/StatisticTable";
-import StepBubbles from "./components/StepBubbles";
-import Table from "./components/Table";
-import Tabs from "./components/Tabs";
-import Text from "./components/Text";
-import TokenAmountInput from "./components/TokenAmountInput";
-import TokenIcon from "./components/TokenIcon";
-import TokenGroup from "./components/TokenGroup";
-import TokenSymbol from "./components/TokenSymbol";
+import Badge, { BadgeProps } from "./components/Badge";
+import Button, { ButtonProps } from "./components/Button";
+import Card, { CardProps } from "./components/Card";
+import Divider, { DividerProps } from "./components/Divider";
+import ExpandableCard, {
+  ExpandableCardProps,
+} from "./components/ExpandableCard";
+import Heading, { HeadingProps } from "./components/Heading";
+import HoverableCard, { HoverableCardProps } from "./components/HoverableCard";
+import Input, { InputProps } from "./components/Input";
+import Link, { LinkProps } from "./components/Link";
+import Modal, { ModalProps } from "./components/Modal";
+import Progress, { ProgressProps } from "./components/Progress";
+import Statistic, { StatisticProps } from "./components/Statistic";
+import StatisticTable, {
+  StatisticTableProps,
+} from "./components/StatisticTable";
+import StepBubbles, { StepBubblesProps } from "./components/StepBubbles";
+import Table, { TableProps } from "./components/Table";
+import Tabs, { TabsProps } from "./components/Tabs";
+import Text, { TextProps } from "./components/Text";
+import TokenAmountInput, {
+  TokenAmountInputProps,
+} from "./components/TokenAmountInput";
+import TokenGroup, { TokenGroupProps } from "./components/TokenGroup";
+import TokenIcon, { TokenIconProps } from "./components/TokenIcon";
+import TokenSymbol, { TokenSymbolProps } from "./components/TokenSymbol";
 
 /**
  * Main entry point to the rari-components package. Typically, you will want
@@ -48,4 +54,27 @@ export {
   TokenIcon,
   TokenGroup,
   TokenSymbol,
+};
+export type {
+  BadgeProps,
+  ButtonProps,
+  CardProps,
+  DividerProps,
+  ExpandableCardProps,
+  HeadingProps,
+  HoverableCardProps,
+  InputProps,
+  LinkProps,
+  ModalProps,
+  ProgressProps,
+  StatisticProps,
+  StatisticTableProps,
+  StepBubblesProps,
+  TableProps,
+  TabsProps,
+  TextProps,
+  TokenAmountInputProps,
+  TokenIconProps,
+  TokenGroupProps,
+  TokenSymbolProps,
 };
